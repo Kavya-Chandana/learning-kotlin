604dcec65d4c2e6972e3ad5a54816cbc9ef4b333
Kavya Chandana <kchandan@gitam.in>
2024-12-10 22:47:53 +0530

    Create functions.kt
    wrote a simple function greet

---
+++ basic/functions.kt
@@ -0,0 +1,25 @@
+/* 
+ * fun functionName(a:Int...):Int{
+ * 
+ * }
+ * 
+ * iLoveKotlin - camel case notation
+ *  used for variables and functions 
+ * ILoveGitam- sentence case notation
+ * used for classes and object constructors 
+ * i_love_gitam -underscore notation
+ * used for other programming languages dart/file names
+ * 
+ * */
+fun main(){
+    println("hello world")
+    greet("manas")
+    greet("kavya")
+    greet("likkiii")
+}
+fun greet(name:String){
+    println("Good morning ${name}. How are u? your name is ${name.length} charaters long ")
+}
+
+
+ 
